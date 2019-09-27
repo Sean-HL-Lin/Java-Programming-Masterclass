0f9bd30fb79a6fd3dd9731bb4766fde3104bdd0d
sean-lin <hongshenglin92@gmail.com>
2019-09-26 20:15:04 -0700

    add hello world class

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastone = 1000 - myTotal;
+        System.out.println(myLastone);
+    }
+
+}
