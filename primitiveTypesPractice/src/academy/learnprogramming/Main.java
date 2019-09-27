dbad72cec0056cc3b9198173671c924db09c8ac2
sean-lin <hongshenglin92@gmail.com>
2019-09-26 21:02:55 -0700

    practice byte short long int

---
+++ primitiveTypesPractice/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longValue = 60000L + 10L *(byteValue + shortValue + intValue);
+    }
+}
