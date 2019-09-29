34167e8252392de05f9affa710aab81df9744972
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-09-28 21:32:36 -0700

    understand char and boolean

---
+++ charAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar ='\u0044';
+        System.out.println(myUnicodeChar);
+        char myCopyrightChat = '\u00A9';
+        System.out.println(myCopyrightChat);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        
+    }
+}
