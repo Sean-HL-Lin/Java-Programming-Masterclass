506968499188c20aa58c748b0aac20f5d7a6ed8c
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-09-30 20:56:52 -0700

    understand block

---
+++ IfAndCodeBlock/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean no = false;
+	    if (no) {
+            System.out.println("no is true");
+        } else {
+            System.out.println("no is false");
+        }
+    }
+}
