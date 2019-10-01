92801f5da68a928ba900adb6343e00070681bffe
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-09-30 20:50:18 -0700

    understand expression, statement and indentation

---
+++ keywordsAndExpression/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    // int int = 60 ---- int is one of 50+ reserved words
+        int highScore = 60;
+        if(highScore == 60) {
+            System.out.println("the score is 60");
+        }
+    }
+}
---
+++ statementWhiteSpaceAndIndentation/src/academy/learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int variable = 50;
+        variable++;
+        variable--;
+        System.out.println("this is a statement");
+        System.out.printf("each line end with ; is a statement " +
+                "event this one");
+    }
+}
