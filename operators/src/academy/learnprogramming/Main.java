d7cfad23a5776cf23fbbb7766d2bf37b12b0df50
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-09-30 20:37:26 -0700

    practice more operators

--- operators/src/academy/learnprogramming/Main.java
+++ operators/src/academy/learnprogramming/Main.java
@@ -14,5 +14,16 @@ public class Main {
         if (isAlien == false) {
             System.out.println("it is not a alien!");
         }
+
+        int topScore = 90;
+        if(80 < topScore && topScore < 100) {
+            System.out.println("print out score");
+        }
+
+        double firstValue = 20;
+        double second = 80;
+        double totalValue = firstValue + second * 100;
+        double remainder = totalValue % 40;
+        System.out.println(remainder);
     }
 }
