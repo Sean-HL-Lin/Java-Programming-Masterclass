b65b13c8408a30fe36d7171f814e8517c100cf6a
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-02 20:56:35 -0700

    understand while

---
+++ While/src/academy/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+	    while(i < 6) {
+            System.out.println("value = " + i);
+	        i++;
+        }
+
+	    int count = 1 ;
+	    do {
+            System.out.println("value = " + count);
+            count++;
+        } while (count < 6 );
+    }
+}
