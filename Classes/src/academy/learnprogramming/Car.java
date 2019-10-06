30e52561932ffc792c920bb9fe941e89fa76b070
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-06 11:12:41 -0700

    understand class

---
+++ Classes/src/academy/learnprogramming/Car.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Car {
+
+    private int door;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+
+}
