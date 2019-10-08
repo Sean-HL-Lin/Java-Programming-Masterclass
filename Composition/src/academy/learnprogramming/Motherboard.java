31e0a1011c97cd114e88517b6f77b8fb17383d94
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-07 21:17:55 -0700

    understand composition

---
+++ Composition/src/academy/learnprogramming/Motherboard.java
@@ -0,0 +1,62 @@
+package academy.learnprogramming;
+
+public class Motherboard {
+    private String Model;
+    private String manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        Model = model;
+        this.manufacturer = manufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName) {
+        System.out.println("program is loading " + programName);
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public void setRamSlots(int ramSlots) {
+        this.ramSlots = ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public void setCardSlots(int cardSlots) {
+        this.cardSlots = cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+
+    public void setBios(String bios) {
+        this.bios = bios;
+    }
+}
+
