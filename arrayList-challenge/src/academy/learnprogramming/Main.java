5f4c44d7bc76d300a7d501312ec0176dd642025f
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-10 13:54:17 -0700

    arrayList exercise

---
+++ arrayList-challenge/src/academy/learnprogramming/Main.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void startPhone() {
+        System.out.println("starting phone.....");
+    }
+
+    private static void printActions() {
+        System.out.println();
+    }
+}
