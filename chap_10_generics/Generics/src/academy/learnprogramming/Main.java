2bc41a8db11bd19821ee692432b911391539e784
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-12 18:31:09 -0700

    understand generic class

---
+++ chap_10_generics/Generics/src/academy/learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+
+        Team<FootballPlayer> teamA = new Team<>("TeamA");
+
+        teamA.addPlayer(joe);
+//        teamA.addPlayer(pat);
+    }
+}
